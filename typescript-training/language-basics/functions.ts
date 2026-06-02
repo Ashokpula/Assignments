0c2bdb7d93d9528106d4f8dd84be45d42110bf48
Pula Ashok <pulaashok99@gmail.com>
2026-06-02 22:54:06 +0530

    Assignment3

---
+++ typescript-training/language-basics/functions.ts
@@ -0,0 +1,13 @@
+//4 types of funtions
+
+//1. Funtion without parameters and wothout return types
+
+//2  Funtion without parameters and with return types
+
+//3  Funtion with parameters and without return types
+
+//4  Funtion with parameters and with return types
+
+//5 Function with optional parameters
+
+//
